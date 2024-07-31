8177222a8f269e37d393788fb34bd08ad7085c72
Ma-Eum <cl.gachelin@gmail.com>
2024-07-31 23:30:05 +0200

    realisation-exo8

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,13 @@
-//Votre code ici
-
- export default add;
+// Définir la fonction `add` qui prend deux paramètres `a` et `b`
+function add(a, b) {
+    // Convertir les paramètres en nombres au cas où ils seraient passés sous forme de chaînes
+    const num1 = parseFloat(a);
+    const num2 = parseFloat(b);
+    
+    // Retourner la somme des deux nombres
+    return num1 + num2;
+  }
+  
+  // Exporter la fonction `add` pour qu'elle puisse être utilisée dans d'autres fichiers
+  export default add;
+  
